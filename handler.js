8c6b9678bd37b333c0049a59afc1e3de4e7a3d57
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-27 20:50:49 +0800

    got basic sending down

--- handler.js
+++ handler.js
@@ -1,4 +1,33 @@
 'use strict';
+const axios = require('axios');
+const moment = require('moment-timezone');
+const env = require('./env.json');
+
+const api = axios.create({
+  baseURL: `https://api.telegram.org/bot${env.token}`,
+  timeout: 1000,
+  headers: { 'X-Custom-Header': 'foobar' },
+});
+
+const getUser = () => {
+  const users = ['@AnFromVietnam', '@ashleyteehee'];
+
+  return users[0];
+};
+
+const getMessage = () => {
+  const user = getUser();
+  const currentDate = moment().tz('Asia/Singapore');
+  const sundayDate = currentDate.add(2, 'd');
+
+  const message = `Hi ${user}, today is ${currentDate.format(
+    'MMMM Do YYYY'
+  )} and it is your turn to clean! Remember to clean the toilet, living room as well as your room by ${sundayDate.format(
+    'MMMM Do YYYY'
+  )}`;
+
+  return message;
+};
 
 module.exports.remind = async (event) => {
   return {
